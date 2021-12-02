2b41a5b2e271d7490d508f3d8fbfe40719a3e4b8
brianchanoocl <Delubdi118>
2021-12-02 18:39:51 +0800

    [fizzBuzz] [Brian] -feat complete fizzbuzz main function

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,19 @@
 package tdd.fizzbuzz;
 
+//[fizzBuzz] [Brian] -feat
+
 public class FizzBuzz {
     public String countoff(int order){
-        
+        String response = "";
+        if(order %3 == 0)
+            response += "Fizz";
+        if(order %5 == 0)
+            response += "Buzz";
+        if(order %7 == 0)
+            response += "Whizz";
+        if(response.length() <= 1)
+            return String.valueOf(order);
+
+        return response;
     }
 }
